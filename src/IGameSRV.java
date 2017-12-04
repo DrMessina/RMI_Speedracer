7b9e98ba51e784b4a9b30daf6abcfec765290a39
DrMessina <33147595+DrMessina@users.noreply.github.com>
2017-12-04 19:21:50 +0100

    boolean

--- src/IGameSRV.java
+++ src/IGameSRV.java
@@ -6,7 +6,12 @@ public interface IGameSRV extends java.rmi.Remote{
 	public void setGui(IGUI gui) throws java.rmi.RemoteException;
 	public void runGame() throws java.rmi.RemoteException;
 	public void score(int score) throws java.rmi.RemoteException;
-	public void bGameQuitTrue() throws java.rmi.RemoteException;
-	public void bGameQuitFalse() throws java.rmi.RemoteException;
+	public void bGameQuit(boolean isQuitting) throws java.rmi.RemoteException;
+	public void bGameFinishing(boolean isFinishing) throws java.rmi.RemoteException;
+	public void bGameInProgress(boolean isProgressing) throws java.rmi.RemoteException;
+	public void UP_P(boolean isUp) throws java.rmi.RemoteException;
+	public void DO_P(boolean isDown) throws java.rmi.RemoteException;
+	public void RI_P(boolean isRight) throws java.rmi.RemoteException;
+	public void LE_P(boolean isLeft) throws java.rmi.RemoteException;
 	public void newGrid() throws java.rmi.RemoteException;
 }
